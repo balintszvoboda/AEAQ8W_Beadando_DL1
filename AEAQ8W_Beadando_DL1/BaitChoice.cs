4068322ed48528214deffecb4bb9da69d22b853a
Balint Szvoboda <aeaq8w@stud.uni-obuda.hu>
2021-05-06 15:30:28 +0200

    Add project files.

---
+++ AEAQ8W_Beadando_DL1/BaitChoice.cs
@@ -0,0 +1,156 @@
+﻿using CNTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AEAQ8W_Beadando_DL1
+{
+    class BaitChoice
+    {
+        const int batchSize = 1000;
+        const int epochCount = 30;
+
+        readonly Variable x;
+        readonly Function y;
+
+        public BaitChoice(int hiddenNeuronCount)
+        {
+            int[] layers = new int[] { DataSet.InputSize, hiddenNeuronCount, hiddenNeuronCount, DataSet.OutputSize };
+
+            // Build graph
+            x = Variable.InputVariable(new int[] { layers[0] }, DataType.Float);
+
+            Function lastLayer = x;
+            for (int i = 0; i < layers.Length - 1; i++)
+            {
+                Parameter weight = new Parameter(new int[] { layers[i + 1], layers[i] }, DataType.Float, CNTKLib.GlorotNormalInitializer());
+                Parameter bias = new Parameter(new int[] { layers[i + 1] }, DataType.Float, CNTKLib.GlorotNormalInitializer());
+
+                Function times = CNTKLib.Times(weight, lastLayer);
+                Function plus = CNTKLib.Plus(times, bias);
+
+                if (i != layers.Length - 2)
+                    lastLayer = CNTKLib.Sigmoid(plus);
+                else
+                    lastLayer = CNTKLib.Softmax(plus);
+            }
+
+            y = lastLayer;
+        }
+
+        public void Train(DataSet ds)
+        {
+            // Extend graph
+            Variable yt = Variable.InputVariable(new int[] { DataSet.OutputSize }, DataType.Float);
+            Function loss = CNTKLib.CrossEntropyWithSoftmax(y, yt);
+            Function err = CNTKLib.ClassificationError(y, yt);
+
+            Learner learner = CNTKLib.SGDLearner(new ParameterVector(y.Parameters().ToArray()), new TrainingParameterScheduleDouble(1.0, batchSize));
+            Trainer trainer = Trainer.CreateTrainer(y, loss, err, new List<Learner>() { learner });
+
+            // Train
+            for (int epochI = 0; epochI <= epochCount; epochI++)
+            {
+                double sumLoss = 0;
+                double sumError = 0;
+
+                ds.Shuffle();
+                for (int batchI = 0; batchI < ds.Count / batchSize; batchI++)
+                {
+                    Value x_value = Value.CreateBatch(x.Shape, ds.Input.GetRange(batchI * batchSize * DataSet.InputSize, batchSize * DataSet.InputSize), DeviceDescriptor.CPUDevice);
+                    Value yt_value = Value.CreateBatch(yt.Shape, ds.Output.GetRange(batchI * batchSize * DataSet.OutputSize, batchSize * DataSet.OutputSize), DeviceDescriptor.CPUDevice);
+                    var inputDataMap = new Dictionary<Variable, Value>()
+                    {
+                        { x, x_value },
+                        { yt, yt_value }
+                    };
+
+                    trainer.TrainMinibatch(inputDataMap, false, DeviceDescriptor.CPUDevice);
+                    sumLoss += trainer.PreviousMinibatchLossAverage() * trainer.PreviousMinibatchSampleCount();
+                    sumError += trainer.PreviousMinibatchEvaluationAverage() * trainer.PreviousMinibatchSampleCount();
+                }
+                Console.WriteLine(String.Format("{0}\t{1:0.0000}\t{2:0.0000}", epochI, sumLoss / ds.Count, 1.0 - sumError / ds.Count));
+            }
+        }
+
+        public void Evaluate(DataSet ds, out double lossValue, out double accValue)
+        {
+            // Extend graph
+            Variable yt = Variable.InputVariable(new int[] { DataSet.OutputSize }, DataType.Float);
+            Function loss = CNTKLib.CrossEntropyWithSoftmax(y, yt);
+            Function err = CNTKLib.ClassificationError(y, yt);
+
+            Evaluator evaluator_loss = CNTKLib.CreateEvaluator(loss);
+            Evaluator evaluator_err = CNTKLib.CreateEvaluator(err);
+
+            double sumEval = 0;
+            double sumLoss = 0;
+            for (int batchI = 0; batchI < ds.Count / batchSize; batchI++)
+            {
+                Value x_value = Value.CreateBatch(x.Shape, ds.Input.GetRange(batchI * batchSize * DataSet.InputSize, batchSize * DataSet.InputSize), DeviceDescriptor.CPUDevice);
+                Value yt_value = Value.CreateBatch(yt.Shape, ds.Output.GetRange(batchI * batchSize * DataSet.OutputSize, batchSize * DataSet.OutputSize), DeviceDescriptor.CPUDevice);
+                var inputDataMap = new UnorderedMapVariableValuePtr()
+                    {
+                        { x, x_value },
+                        { yt, yt_value }
+                    };
+
+                sumLoss += evaluator_loss.TestMinibatch(inputDataMap) * batchSize;
+                sumEval += evaluator_err.TestMinibatch(inputDataMap) * batchSize;
+            }
+            lossValue = sumLoss / ds.Count;
+            accValue = 1 - sumEval / ds.Count;
+        }
+
+        public String VisibleTest(DataSet ds, int count)
+        {
+            Value x_value = Value.CreateBatch(x.Shape, ds.Input.GetRange(0, count * DataSet.InputSize), DeviceDescriptor.CPUDevice);
+            var inputDataMap = new UnorderedMapVariableValuePtr()
+                    {
+                        { x, x_value }
+                    };
+            var outputDataMap = new UnorderedMapVariableValuePtr()
+                    {
+                        { y, null }
+                    };
+
+            y.Evaluate(inputDataMap, outputDataMap, DeviceDescriptor.CPUDevice);
+            IList<IList<float>> resultValue = outputDataMap[y].GetDenseData<float>(y);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(ds.DataToString(i));
+                sb.Append("Result:[");
+                int max = 0;
+                for (int d = 0; d <= 5; d++)
+                {
+                    if (resultValue[i][d] > resultValue[i][max])
+                        max = d;
+                    if (d != 0) sb.Append(", ");
+                    sb.Append(String.Format("{0:0.00}", resultValue[i][d]));
+                }
+                sb.Append("]\nPrediction:").Append(max).Append("\n\n");
+            }
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            int hiddenNeuronCount = 300;
+
+            DataSet.LoadMinMax(@"..\..\..\..\..\Data\baits_train_m.txt");
+            var trainDS = new DataSet(@"..\..\..\..\..\Data\baits_train_m.txt");
+            var testDS = new DataSet(@"..\..\..\..\..\Data\baits_test.txt");
+
+            var app = new BaitChoice(hiddenNeuronCount);
+            app.Train(trainDS);
+            app.Evaluate(trainDS, out double trainLoss, out double trainAcc);
+            app.Evaluate(testDS, out double testLoss, out double testAcc);
+            Console.WriteLine(String.Format("Final evaluation: {0}\t{1:0.0000}\t{2:0.0000}\t{3:0.0000}\t{4:0.0000}\n", hiddenNeuronCount, trainLoss, trainAcc, testLoss, testAcc));
+            Console.WriteLine(app.VisibleTest(trainDS, 10));
+            //Console.WriteLine(trainDS.DataToString(0));
+        }
+    }
+}
---
+++ AEAQ8W_Beadando_DL1/DataSet.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AEAQ8W_Beadando_DL1
+{
+    public class DataSet
+    {
+        public const int InputSize = 5;
+        public List<float> Input { get; set; } = new List<float>();
+
+        public const int OutputSize = 6;
+        public List<float> Output { get; set; } = new List<float>();
+
+        public int Count { get; set; }
+
+        public DataSet(string filename)
+        {
+            LoadData(filename);
+        }
+
+        void LoadData(string filename)
+        {
+            Count = 0;
+            foreach (String line in File.ReadAllLines(filename))
+            {
+                var data = Normalize(line.Split('\t').Select(x => float.Parse(x)).ToList());
+                Input.AddRange(data.GetRange(0, InputSize));
+                for (int i = 0; i <= 5; i++)
+                {
+                    Output.Add(data[InputSize] == i ? 1.0f : 0.0f);
+                }
+                Count++;
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random rnd = new Random();
+            for (int swapI = 0; swapI < Count; swapI++)
+            {
+                var a = rnd.Next(Count);
+                var b = rnd.Next(Count);
+                if (a != b)
+                {
+                    float T;
+                    for (int i = 0; i < InputSize; i++)
+                    {
+                        T = Input[a * InputSize + i];
+                        Input[a * InputSize + i] = Input[b * InputSize + i];
+                        Input[b * InputSize + i] = T;
+                    }
+                    for (int i = 0; i < OutputSize; i++)
+                    {
+                        T = Output[a * OutputSize + i];
+                        Output[a * OutputSize + i] = Output[b * OutputSize + i];
+                        Output[b * OutputSize + i] = T;
+                    }
+                }
+            }
+        }
+
+        static float[] minValues;
+        static float[] maxValues;
+        public static List<float> Normalize(List<float> floats)
+        {
+            List<float> normalized = new List<float>();
+            for (int i = 0; i < floats.Count-1; i++)
+                normalized.Add((floats[i] - minValues[i]) / (maxValues[i] - minValues[i]));
+            normalized.Add(floats.Last());
+            return normalized;
+        }
+
+        public static void LoadMinMax(string filename)
+        {
+            foreach (String line in File.ReadAllLines(filename))
+            {
+                var floats = line.Split('\t').Select(x => float.Parse(x)).ToList();
+                if (minValues == null)
+                {
+                    minValues = floats.ToArray();
+                    maxValues = floats.ToArray();
+                }
+                else
+                {
+                    for (int i = 0; i < floats.Count; i++)
+                        if (floats[i] < minValues[i])
+                            minValues[i] = floats[i];
+                        else
+                            if (floats[i] > maxValues[i])
+                            maxValues[i] = floats[i];
+                }
+            }
+        }
+
+        public String DataToString(int index)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(index+".\nInput: [");
+            for (int d = 0; d <= 4; d++)
+            {
+                if (d != 0) sb.Append(", ");
+                sb.Append(String.Format("{0:0.00}", Input[index * InputSize + d]));
+            }
+            sb.Append("]\tOutput: [");
+            for (int d = 0; d <= 5; d++)
+            {
+                if (d != 0) sb.Append(", ");
+                sb.Append(String.Format("{0:0.00}", Output[index * OutputSize + d]));
+            }
+            sb.Append("]\n");
+            return sb.ToString();
+        }
+    }
+}
